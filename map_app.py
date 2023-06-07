1239f5782475fc266034249bd5578fe567b6ac2a
magnesyljuasen <msylju@gmail.com>
2023-06-07 16:36:08 +0200

    html feil

--- map_app.py
+++ map_app.py
@@ -4,20 +4,16 @@ import requests
 from jose import jwt
 from cognite.client import ClientConfig, CogniteClient
 from cognite.client.credentials import Token
+from _map import Map
+from folium import IFrame
 
-# A standalone example of using keycloak to authenticate to cdf and verify that we are logged inn
 
-project = "energima"
-server_url = "https://auth.properate.com"
+#--
+import streamlit as st
+from streamlit_extras.chart_container import chart_container
+import plotly.express as px
 
-realm = {
-    "energima": "prod",
-    "energima-dev": "dev",
-    "energima-test": "test"
-}
-
-_last_token = None
-
+#-------
 def token_expired(token):
     header = jwt.get_unverified_claims(token)
     exp = header.get("exp")
@@ -48,8 +44,20 @@ def token():
         _last_token = request_token()
     return _last_token
 
-#client_name = os.environ.get("asplan_viak", "read_all")
-#client_secret = os.environ.get("xdevIMhUEpqWjGxjzELJ0Xfof8QVfktx")
+
+#-------
+
+# A standalone example of using keycloak to authenticate to cdf and verify that we are logged inn
+project = "energima"
+server_url = "https://auth.properate.com"
+
+realm = {
+    "energima": "prod",
+    "energima-dev": "dev",
+    "energima-test": "test"
+}
+
+_last_token = None
 
 client_name="asplan_viak"
 client_secret = "xdevIMhUEpqWjGxjzELJ0Xfof8QVfktx"
@@ -57,8 +65,24 @@ client_secret = "xdevIMhUEpqWjGxjzELJ0Xfof8QVfktx"
 cdf_client_name = os.environ.get("AH_7224_Gammelbakkan_15", client_name)
 
 cfg = ClientConfig(client_name=cdf_client_name, project=project,
-                   credentials=Token(token))
+                credentials=Token(token))
 cdf = CogniteClient(cfg)
-a = cdf.time_series.retrieve(external_id="TS_7224_Gammelbakkan_15+GB15=320.002-RT401").metadata
-print(a)
+#--------------------------------------------------------------------------------------------------------------
+external_id_list = cdf.time_series.list().to_pandas()["external_id"]
+#
+selected_time_series = external_id_list[4]
+df = cdf.time_series.data.retrieve(external_id=selected_time_series).to_pandas()
+metadata = cdf.time_series.retrieve(external_id=selected_time_series).to_pandas().T
 
+fig =px.scatter(x=range(10), y=range(10))
+fig.write_html("figur.html")
+figure_data = df[selected_time_series][-5:].to_numpy()
+
+
+# Kart 
+map_obj = Map()
+map_obj.address_lat = 63.28629
+map_obj.address_long = 10.26335
+map_obj.address_name = "Gammelbakkan 15"
+map_obj.create_wms_map(selected_zoom = 17, figure_data = figure_data)
+map_obj.show_map()
